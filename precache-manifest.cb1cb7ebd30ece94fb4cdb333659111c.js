713a496330cec056c37f525782af00ec8509eadb
GlauberGoncalves <sistemas.glauber@gmail.com>
2020-09-30 19:28:28 -0300

    Updates

---
+++ precache-manifest.cb1cb7ebd30ece94fb4cdb333659111c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf7319a2d32efcfe4842a367ec85067b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8866150042931382e90e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "039c41c5489c079c4f51",
+    "url": "/static/js/2.ba88fab7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba88fab7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8866150042931382e90e",
+    "url": "/static/js/main.889d2a38.chunk.js"
+  },
+  {
+    "revision": "39ce663ce569126f5183",
+    "url": "/static/js/runtime-main.800cff1d.js"
+  }
+]);
